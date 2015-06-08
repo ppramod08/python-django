604970c1a29638681f662a1cba844380bd25b603
adambure21 <adambure21@gmail.com>
2015-06-08 12:18:56 +0530

    Added Addition function in calculator program

---
+++ python_basic/calculator.py
@@ -0,0 +1,12 @@
+#usr/
+number_1 = int(raw_input("Enter first number : "))
+number_2 = int(raw_input("Ennter second number : "))
+
+def addition(number_1, number_2):
+    """
+    Returns addition of two number
+
+    """
+    return number_1+number_2
+
+print addition(number_1,number_2)
